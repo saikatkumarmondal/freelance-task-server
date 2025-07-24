910b3464bf9f416ae6703d98b0fdef6fc9854941
saikat <mondalsaikatkumar@gmail.com>
2025-07-24 12:19:02 +0600

    create post, get api

---
+++ index.js
@@ -0,0 +1,62 @@
+const cors = require( 'cors' );
+require( 'dotenv' ).config();
+const express = require( 'express' );
+const app = express();
+
+app.use( cors() );
+app.use( express.json() );
+const port = process.env.PORT || 7777;
+
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_CLUSTER}/?retryWrites=true&w=majority&appName=${process.env.DB_APPNAME}`;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run () {
+  //HZLqyrX4N1KZTzBY
+  //simpleDbUser
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+ const userCollection = client.db( 'freelancerapp' ).collection( 'users' );
+ 
+
+
+
+    await client.db( "admin" ).command( { ping: 1 } );
+       // user's APIS
+    app.post( '/users', async ( req, res ) => {
+      const user = req.body;
+      const result = await userCollection.insertOne( user );
+      res.send( result );
+    } )
+    
+    app.get( '/users/:email', async ( req, res ) => {
+      const email = req.params.email;
+      const filter = { email: email };
+      const result = await userCollection.findOne( filter );
+      res.send( result );
+    } );
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    //await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.listen( port, () => {
+    console.log(`Server started on ${port}`);
+})
